c10b5938c9e6028ce2c4a264b3e72eab7bd054f9
IsoEntity <xynfamail@gmail.com>
2018-04-05 14:21:27 +0200

    now functional

--- index.js
+++ index.js
@@ -5,101 +5,94 @@ const path = require("path");
 // community modules
 const Promise = require("promise");
 
-class ConfigLoader {
-
-    constructor(files) {
-        this._loadFiles(files, __dirname, this);
-    }
-
-    _checkDirectory(directory) {
-        return new Promise((resolve, reject) => {
-            fs.access(directory, (error) => {
-                if (error) {
-                    fs.mkdir(directory, (error) => {
-                        if (error) {
-                            reject(error);
-                        } else {
-                            resolve();
-                        }
-                    });
-                } else {
-                    resolve();
-                }
-            });
-        });
-    }
-
-    async _loadFile(file, defaults, object) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                await this._checkDirectory(path.dirname(file));
-            } catch (error) {
-                reject(error);
-            }
-
-            const name = path.basename(file, path.extname(file));
-
-            fs.access(file, (error) => {
-                if (error) {
-                    const data = defaults instanceof Object ? JSON.stringify(defaults, null, 4) : defaults;
-
-                    fs.writeFile(file, data, (error) => {
-                        if (error) {
-                            reject(error);
-                        } else {
-                            object[name] = defaults;
-                            resolve();
-                        }
-                    });
-                } else {
-                    fs.readFile(file, (error, data) => {
-                        if (error) {
-                            reject(error);
-                        } else {
-                            try {
-                                object[name] = JSON.parse(data.toString());
-                            } catch (error) {
-                                object[name] = data.toString();
-                            }
-
-                            resolve();
-                        }
-                    });
-                }
-            });
-        });
-    }
-
-    async _loadFiles(files, directory, object) {
-        return new Promise(async (resolve, reject) => {
-            for (const key in files) {
-                const defaults = files[key];
-                const keyFile = path.resolve(directory, key);
-
-                if (defaults instanceof Object && !path.extname(key)) {
-                    // it's a directory
-                    object[key] = {};
-
-                    try {
-                        await this._loadFiles(defaults, keyFile, object[key]);
-                    } catch (error) {
+function checkDirectory(directory) {
+    return new Promise((resolve, reject) => {
+        fs.access(directory, (error) => {
+            if (error) {
+                fs.mkdir(directory, (error) => {
+                    if (error) {
                         reject(error);
+                    } else {
+                        resolve();
                     }
-                } else {
-                    // it's a file
-                    try {
-                        await this._loadFile(keyFile, defaults, object);
-                    } catch (error) {
-                        reject(error);
-                    }
-                }
-
+                });
+            } else {
                 resolve();
             }
         });
-    }
-
+    });
 }
 
-// export
-module.exports = ConfigLoader;
+async function loadFile(file, defaults) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            await checkDirectory(path.dirname(file));
+        } catch (error) {
+            reject(error);
+        }
+
+        fs.access(file, (error) => {
+            if (error) {
+                const data = defaults instanceof Object ? JSON.stringify(defaults, null, 4) : defaults;
+
+                fs.writeFile(file, data, (error) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(defaults);
+                    }
+                });
+            } else {
+                fs.readFile(file, (error, data) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        try {
+                            resolve(JSON.parse(data.toString()));
+                        } catch (error) {
+                            resolve(data.toString());
+                        }
+                    }
+                });
+            }
+        });
+    });
+}
+
+async function loadFiles(files, directory) {
+    return new Promise(async (resolve, reject) => {
+        const object = {};
+
+        for (const key in files) {
+            const defaults = files[key];
+            const keyFile = path.resolve(directory, key);
+
+            if (defaults instanceof Object && !path.extname(key)) {
+                // it's a directory
+                try {
+                    object[key] = await loadFiles(defaults, keyFile);
+                } catch (error) {
+                    reject(error);
+                }
+            } else {
+                // it's a file
+                const name = path.basename(key, path.extname(key));
+
+                try {
+                    object[name] = await loadFile(keyFile, defaults);
+                } catch (error) {
+                    reject(error);
+                }
+            }
+        }
+
+        resolve(object);
+    });
+}
+
+function load(files) {
+    return loadFiles(files, __dirname);
+}
+
+// exports
+module.exports = load;
